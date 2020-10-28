611ed7ea58f1970bfc20b1a280c4e03df3cfaf0a
alaa140101 <alaa140011@gmail.com>
2020-10-28 15:22:47 +0300

    Save Favourites

--- script.js
+++ script.js
@@ -5,26 +5,30 @@ const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
 // NASA API
-const page = 1;
+const count = 10;
 const apiKey = 'DEMO_KEY';
-const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${page}&api_key=${apiKey}`;
+//const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${page}&api_key=${apiKey}`;
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
-  resultsArray.photos.forEach((result) => {
-    console.log(result.img_src);
+  resultsArray.forEach((result) => {
+  //resultsArray.photos.forEach((result) => {
     // card container
     const card = document.createElement('div');
     card.classList.add('card');
     // link
     const link = document.createElement('a');
-    link.href = result.hdurl;
+    link.href = result.url;
     link.title = 'View Full Image';
     link.target = '_blank';
     // image
     const img = document.createElement('img');
     img.classList.add('card-img-top');
-    img.src = result.img_src;
+    img.src = result.url;
+    //img.src = result.img_src;
     img.alt = 'Mars Rover Photos';
     img.loading = 'lazy';
     // Card body
@@ -33,23 +37,36 @@ function updateDOM() {
     // Title
     const cardTitle = document.createElement('h5');
     cardTitle.classList.add('card-title');
-    cardTitle.textContent = result.rover.name;
-    // Save
+    cardTitle.textContent = result.title;
+    //cardTitle.textContent = result.rover.name;
+    // Save Text
     const saveText = document.createElement('p');
     saveText.classList.add('clickable');
     saveText.textContent = 'Add to Favorates';
-    // footer
+    //saveText.onclick = `saveFavorite('${result.url}')`;
+    saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+    //console.log(`'${result.url}'`);
+    //saveText.onclick = `saveFaverite('${result.img_src}')`
+    // Card Text
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    // Footer Container
     const footer = document.createElement('small');
     footer.classList.add('text-muted');
     // Date
     const date = document.createElement('strong');
-    date.textContent = result.earth_date;
+    date.textContent = result.date;
+    // Copyright
+    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+    const copyright = document.createElement('span');
+    copyright.textContent = `${copyrightResult}`;
+    //date.textContent = result.earth_date;
     // Append
-    footer.appendChild(date);
-    cardBody.append(cardTitle, saveText, footer);
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
     link.appendChild(img);
     card.append(link, cardBody);
-    //console.log(card);
+    console.log(copyright);
     imagesContainer.appendChild(card);
   });
 }
@@ -60,10 +77,28 @@ async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
+    console.log(resultsArray);
     updateDOM();
   } catch (error) {
     console.log(error);
   }
+}
+
+// Add result to Favorites
+function saveFavorite(itemUrl) {
+  // Loop through Results Array to select Favorite
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      console.log(favorites);
+      // Show save confirmation for 2 seconds
+      saveConfirmed.hidden = false;
+      setTimeout(() => {
+        saveConfirmed.hidden = true;
+      }, 2000);
+      localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+    }
+  });
 }
 
 getNasaPictures()
